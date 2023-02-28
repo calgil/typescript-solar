9515e476a61cfbc3930df9ec640da4605a8ded78
calgil <clgilbert1105@gmail.com>
2023-02-28 14:15:04 -0700

    solve ex11

--- exercises/e11.ts
+++ exercises/e11.ts
@@ -1,7 +1,17 @@
 // SPACE DATA EXERCISE 11
 // Return an array of Planets with less than 10 moons
+
+import { Planet } from "../data/data";
+
 // include planets with 0 moons
-export function lowMoonsPlanets(planets) {}
+export function lowMoonsPlanets(planets: Planet[]) {
+  return planets.reduce((acc, planet) => {
+    if (!planet.moonsCount || planet.moonsCount < 10) {
+      return [...acc, planet];
+    }
+    return acc;
+  }, [] as Planet[]);
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-11"
