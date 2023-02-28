f0bc784d1db488c914cc7136638a8c4dfc9586c8
calgil <clgilbert1105@gmail.com>
2023-02-28 14:05:54 -0700

    solve ex10

--- exercises/e10.ts
+++ exercises/e10.ts
@@ -1,7 +1,18 @@
 // SPACE DATA EXERCISE 10
 // Return a given asteroid object of data
+
+import { AllData } from "../data/data";
+
 // make sure that you destructure your parameters
-export function getAsteroidDataByName({ allData, asteroidName }) {}
+export function getAsteroidDataByName({
+  allData,
+  asteroidName,
+}: {
+  allData: AllData;
+  asteroidName: String;
+}) {
+  return allData.asteroids.find((asteroid) => asteroid.name === asteroidName);
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-10"
