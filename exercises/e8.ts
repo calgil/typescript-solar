6b29afafe7596137aaa679ff830cfc4ed409e931
calgil <clgilbert1105@gmail.com>
2023-02-28 13:58:28 -0700

    solve ex8

--- exercises/e8.ts
+++ exercises/e8.ts
@@ -1,7 +1,28 @@
 // SPACE DATA EXERCISE 8
 // Return a Planet by a given moon name
+
+import { Planet } from "../data/data";
+
 //  must have destructured parameters
-export function findPlanetByMoon({ planets, moonName }) {}
+export function findPlanetByMoon({
+  planets,
+  moonName,
+}: {
+  planets: Planet[];
+  moonName: String;
+}) {
+  let planetWithMoon;
+  planets.map((planet) => {
+    if (planet.moons) {
+      planet.moons.map((moon) => {
+        if (moon.toLowerCase() === moonName.toLowerCase()) {
+          planetWithMoon = planet;
+        }
+      });
+    }
+  });
+  return planetWithMoon;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
